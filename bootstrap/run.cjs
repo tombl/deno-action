8b7d5ae81fb14f2fd924653ecbe3a42a2b7f1c9a
Thomas Stokes <git@tombl.dev>
2024-07-09 23:56:46 +0800

    Silence curl

--- bootstrap/run.cjs
+++ bootstrap/run.cjs
@@ -34,7 +34,7 @@ $url = "https://github.com/denoland/deno/releases/download/v1.44.4/deno-$target.
 
 if (-not (Test-Path -Path "$cache.complete")) {
   echo "Downloading Deno..."
-  curl.exe -L $url -o $zip
+  curl.exe --silent -L $url -o $zip
   tar.exe -xf $zip -C $cache
   Remove-Item -Force $zip
   $null = New-Item -ItemType File -Force -Path "$cache.complete"
@@ -67,7 +67,7 @@ url="https://github.com/denoland/deno/releases/download/${DENO_VERSION}/deno-\${
 
 if [ ! -f "$cache.complete" ]; then
   echo "Downloading Deno..."
-  curl --fail --location "$url" --output "$zip"
+  curl --silent --fail --location "$url" --output "$zip"
   unzip -q -o -d "$cache" "$zip"
   rm -f "$zip"
   touch "$cache.complete"
