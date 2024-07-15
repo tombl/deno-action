7cf64127e16206cecaef7a6f5439b9d3bc222f24
Thomas Stokes <git@tombl.dev>
2024-07-15 19:07:19 +0800

    try using bash on windows

--- bootstrap/run.cjs
+++ bootstrap/run.cjs
@@ -18,51 +18,24 @@ if (!process.env.RUNNER_TOOL_CACHE || !process.env.RUNNER_TEMP) {
 }
 
 module.exports = (file) => {
-  const [command, ...args] = process.platform === "win32"
-    ? [
-      "powershell",
-      "-Command",
-      `
-$ErrorActionPreference = "Stop"
-
-$cache = "$env:RUNNER_TOOL_CACHE\\deno-action\\${DENO_VERSION}\\${process.arch}"
-$null = New-Item -ItemType Directory -Force -Path $cache
-$zip = "$env:RUNNER_TEMP\\deno-${DENO_VERSION}.zip"
-
-$target = "x86_64-pc-windows-msvc"
-$url = "https://github.com/denoland/deno/releases/download/v1.44.4/deno-$target.zip"
-
-if (-not (Test-Path -Path "$cache.complete")) {
-  echo "Downloading Deno..."
-  curl.exe --silent -L $url -o $zip
-  tar.exe -xf $zip -C $cache
-  Remove-Item -Force $zip
-  $null = New-Item -ItemType File -Force -Path "$cache.complete"
-}
-
-$env:DENO_DIR = "$env:RUNNER_TEMP\\.deno"
-
-& "$cache\\deno.exe" run --quiet --no-prompt --allow-all '${file}'
-exit $LastExitCode
-    `,
-    ]
-    : [
-      "sh",
-      "-c",
-      `
+  const child = spawnSync("bash", [
+    "-c",
+    `
 set -e
 
 cache="$RUNNER_TOOL_CACHE/deno-action/${DENO_VERSION}/${process.arch}"
 mkdir -p "$cache"
 zip="$RUNNER_TEMP/deno-${DENO_VERSION}.zip"
 
-case $(uname -sm) in
-"Darwin x86_64")  target="x86_64-apple-darwin";;
-"Darwin arm64")   target="aarch64-apple-darwin" ;;
-"Linux aarch64")  target="aarch64-unknown-linux-gnu" ;;
-"Linux x86_64")   target="x86_64-unknown-linux-gnu" ;;
+case $RUNNER_OS-$(uname -m); in
+Windows-x86_64) target="x86_64-pc-windows-msvc";;
+Darwin-x86_64)  target="x86_64-apple-darwin";;
+Darwin-arm64)   target="aarch64-apple-darwin" ;;
+Linux-aarch64)  target="aarch64-unknown-linux-gnu" ;;
+Linux-x86_64)   target="x86_64-unknown-linux-gnu" ;;
 *) echo "Unsupported os/architecture"; exit 1 ;;
 esac
+
 url="https://github.com/denoland/deno/releases/download/${DENO_VERSION}/deno-\${target}.zip"
 
 if [ ! -f "$cache.complete" ]; then
@@ -78,9 +51,7 @@ export DENO_DIR="$RUNNER_TEMP/.deno"
 chmod +x "$cache/deno"
 exec "$cache/deno" run --quiet --no-prompt --allow-all '${file}'
     `,
-    ];
-
-  const child = spawnSync(command, args, { stdio: "inherit" });
+  ], { stdio: "inherit" });
   if (child.error) throw child.error;
   process.exitCode = child.status;
 };
