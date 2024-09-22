2b92e7910f7db64f723a9b156e2770aac650e397
Thomas Stokes <git@tombl.dev>
2024-09-22 23:26:11 +0800

    log the resolved version of deno

--- bootstrap/run.mjs
+++ bootstrap/run.mjs
@@ -65,7 +65,7 @@ mkdir -p "$cache"
 zip="$RUNNER_TEMP/deno-${version}.zip"
 
 if [ ! -f "$cache.complete" ]; then
-  echo "::debug::Downloading Deno"
+  echo "::debug::Downloading Deno ${version}"
   curl --silent --fail --location '${url}' --output "$zip"
   unzip -q -o -d "$cache" "$zip"
   rm -f "$zip"
